e9557087220bf0fb073847f240810325d2ef496c
Giselle Ferreira <giselle.fs@gmail.com>
2024-07-26 14:56:41 -0300

    Initial structure

---
+++ src/main/java/giselletech/dio_java_spring_railway/Application.java
@@ -0,0 +1,13 @@
+package giselletech.dio_java_spring_railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/giselletech/dio_java_spring_railway/DioJavaSpringRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package giselletech.dio_java_spring_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioJavaSpringRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
