0995ad12d2ed94e2a72bea6654559d210f265e2d
Giselle Ferreira <giselle.fs@gmail.com>
2024-07-29 03:42:39 -0300

    adds annotation to fix cors problem

--- src/main/java/giselletech/dio_java_spring_railway/Application.java
+++ src/main/java/giselletech/dio_java_spring_railway/Application.java
@@ -1,8 +1,11 @@
 package giselletech.dio_java_spring_railway;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
